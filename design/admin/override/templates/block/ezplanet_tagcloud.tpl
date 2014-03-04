6d429f49f622f32f327cc3db53908362d91443d0
Guillaume Kulakowski <guillaume@llaumgui.com>
2014-03-04 13:59:50 +0100

    Init bundle structure

--- design/admin/override/templates/block/ezplanet_tagcloud.tpl
+++
@@ -1,1 +0,0 @@
-<h2>{'Block:'|i18n( 'design/standard/block/view' )} {ezini( $block.type, 'Name', 'block.ini' )}</h2>
--- settings/content.ini.append.php
+++
@@ -1,8 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[VersionManagement]
-
-# Store only 1 version for a blog post
-VersionHistoryClass[planet_blogpost]=1
-
-*/ ?>
